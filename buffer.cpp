ec50b794c5fb54562f7599f135a94a81ed37d1e1
kais <47930630+lKaisl@users.noreply.github.com>
2021-06-12 22:52:03 -0400

    Add files via upload
    Simple buffer overflow demonstration in C++

---
+++ buffer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char BUFFER_SIZE [20];
+
+// void credintials()
+// {
+//     string user = "Admin";
+//     string server_key = "SuperSecretPassword";
+//     string user_info [4] = {"1234 Main St", "123-456-7890", "11111", "Male"  };
+
+// }
+
+int main()
+{
+    cout<<"Enter your name/n" ;
+    cin>>BUFFER_SIZE;
+    cout<<@BUFFER_SIZE;
+
+}
+
+
